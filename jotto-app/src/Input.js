2f96d5cbb7f6fcddfcc01dabcf6e96312dd3fb32
AhmedAGadir <ahmed.gadir@gmail.com>
2020-01-29 14:46:05 +0000

    first mock function test done

--- jotto-app/src/Input.js
+++ jotto-app/src/Input.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 function Input(props) {
+    // not destructuring useState from React so that we can mock
+    const [currentGuess, setCurrentGuess] = React.useState('');
     return (
-        <div data-test="component-input"></div>
+        <div data-test="component-input">
+            <form className="form-inline">
+                <input
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    type="text"
+                    placeholder="enter guess"
+                    value={currentGuess}
+                    onChange={event => setCurrentGuess(event.target.value)} />
+                <button
+                    data-test="submit-button"
+                    onClick={e => e.preventDefault()}
+                    className="btn btn-primary mb-2"
+                >Submit</button>
+            </form>
+        </div>
     );
 }
 
--- jotto-app/src/Input.test.js
+++ jotto-app/src/Input.test.js
@@ -8,9 +8,8 @@ import Input from './Input';
  * @function setup
  * @returns {ShallowWrapper}
  * */
-const setup = (props = {}) => {
-
-    return shallow(<Input {...props} />);
+const setup = (secretWord = 'party') => {
+    return shallow(<Input secretWord={secretWord} />);
 };
 
 
@@ -18,4 +17,23 @@ test('App renders without error', () => {
     const wrapper = setup();
     const appComponent = findByTestAttr(wrapper, 'component-input');
     expect(appComponent.length).toBe(1);
+});
+
+describe('state controlled input field', () => {
+    test('state updates with value of input box upon change', () => {
+        // jest.fn here creates a function that we can watch - how many times its been called, what its been called with etc
+        // this specific one has no implementation, were just testing that its getting called with the right params
+        const mockSetCurrentGuess = jest.fn();
+        // here jest.fn does have an implementation, were overriding React.useState to be a new function that returns an empty string
+        // for the initial state and our other mock function for the method that updates the state
+        React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'train' } };
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    })
 });
